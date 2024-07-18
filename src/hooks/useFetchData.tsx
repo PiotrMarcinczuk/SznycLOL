08a1a35aa30fff2097a9f945b7111a6a64557f1e
Piotr Marcinczuk <piotr.marcinczuk@gmail.com>
2024-07-18 11:47:27 +0000

    Initial commit

---
+++ src/hooks/useFetchData.tsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import useLocalStorageData from "./useLocalStorageData";
+import { http } from "../app/config";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+export default function useFetchData() {
+  const router = useRouter();
+  const [userData, setUserData] = useState({
+    nickname: typeof window !== 'undefined' ? localStorage.getItem("nickname") || "" : "",
+    tag: typeof window !== 'undefined' ? localStorage.getItem("nickname") || "" : "",
+    puuid: "",
+  });
+  const { setData } = useLocalStorageData();
+
+  const fetchAccessData = async (
+    nickname: string,
+    tag: string,
+    setValidating: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    axios
+      .post(`${http}/getPuuid`, {
+        nickname: nickname,
+        tag: tag,
+      })
+      .then((response) => {
+        setUserData({
+          nickname: response.data.gameName,
+          tag: response.data.tagLine,
+          puuid: response.data.puuid,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        if (error.response.status === 403) setValidating(true);
+        return;
+      });
+  };
+
+  const fetchChampionMastery = async (puuid: string, region: string) => {
+    axios
+      .post(`${http}/getChampionMastery`, {
+        puuid: puuid,
+        region: region,
+      })
+      .then((response) => {
+        router.refresh();
+        console.log(response.data);
+        if (typeof window !== 'undefined') {setData(response);}
+        
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        return;
+      });
+  };
+
+  return {
+    fetchAccessData,
+    fetchChampionMastery,
+    userData,
+  };
+}
