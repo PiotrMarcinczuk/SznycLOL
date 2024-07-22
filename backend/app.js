1fe5809c2ff9b7de06688db72b266c16552afbe4
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-07-22 16:38:51 +0200

    mobile css

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const allowedOrigins = ["https://sznyclol.fun", "https://www.sznyclol.fun"];
+
 app.use(
   cors({
-    origin: "http://sznyclol.fun",
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
