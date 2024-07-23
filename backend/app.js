63ae7b2e0bea4369602f3d5490554d572d4e8d24
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-07-23 16:35:48 +0200

    mobile for chart

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const allowedOrigins = ["https://sznyclol.fun", "https://www.sznyclol.fun"];
-
 app.use(
   cors({
-<<<<<<< Updated upstream
     origin: function (origin, callback) {
       // Allow requests with no origin, like mobile apps or curl requests
       if (!origin) return callback(null, true);
@@ -21,9 +19,7 @@ app.use(
         return callback(new Error("Not allowed by CORS"));
       }
     },
-=======
-    origin: "http://sznyclol.fun",
->>>>>>> Stashed changes
+
     credentials: true,
   })
 );
