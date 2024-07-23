c6fbf1089f902b666819f16984db3f45c3079848
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-07-23 18:41:25 +0200

    url icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,20 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <title>SznycLOL</title>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+        <link
+          rel="icon"
+          href="../../public/sznyc_lol_logo.png"
+          type="image/png"
+        />
+        <link
+          rel="shortcut icon"
+          href="../../public/sznyc_lol_logo.png"
+          type="image/png"
         />
       </head>
       <body>
