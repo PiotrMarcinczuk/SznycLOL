e5f60588d4219b2edcdbe236567de9df951b2d14
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-07-24 13:13:22 +0200

    error during fetch mastery

--- src/app/[nickname]/[tag]/[server]/page.tsx
+++ src/app/[nickname]/[tag]/[server]/page.tsx
@@ -14,44 +14,29 @@ interface MainContentProps {
 export default function MainContent({ params }: MainContentProps) {
   const { getData } = useLocalStorageData();
   const [valuableChampions, setValuableChampions] = useState([]);
-  const [errorDuringFetch, setErrorDuringFetch] = useState(false);
+
   useEffect(() => {
     let championsMastery = null;
     if (typeof window !== "undefined") {
       championsMastery = getData();
     }
     if (championsMastery) {
-      setErrorDuringFetch(false);
       const filteredMastery = championsMastery
         .filter((el: any) => el.championPoints >= 12000)
         .map((el: any) => el);
       setValuableChampions(filteredMastery);
     }
-    setTimeout(() => {
-      if (!championsMastery) {
-        setErrorDuringFetch(true);
-      }
-    }, 2000);
   }, [params]);
 
   return (
     <>
       <Header initialNickname={params.nickname} initialTag={params.tag} />
-      {errorDuringFetch ? (
-        <>
-          <p>ERROR</p>
-          <span>
-            Couldn't find summoner "{params.nickname.split("%20").join(" ")}"
-            and tag #{params.tag}
-          </span>
-        </>
-      ) : (
-        <BubbleChart
-          data={valuableChampions}
-          nickname={params.nickname}
-          tag={params.tag}
-        />
-      )}
+
+      <BubbleChart
+        data={valuableChampions}
+        nickname={params.nickname}
+        tag={params.tag}
+      />
     </>
   );
 }
